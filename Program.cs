c054ddfb5013198252f578fc525a2605e6ae7eaf
LuanGCMenezes <luangui2006@gmail.com>
2025-05-14 17:40:15 -0300

    Resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+string resposta; 
+
+
+Console.WriteLine("O objeto está se movendo (S/N)?");
+bool movendo = Console.ReadLine()!.ToUpper().Substring(0, 1) == "S";
+
+Console.WriteLine("Deveria(S/N)?");
+bool deveria = Console.ReadLine()!.ToUpper().Substring(0, 1) == "N";
+
+
+if (!movendo)
+{
+
+    if(!deveria)
+    {
+        resposta = "ótimo";
+        Console.WriteLine(resposta);
+   } 
+   else
+   {
+    resposta = "Use WD-40.";
+     Console.WriteLine(resposta);
+   }
+}
+else
+{
+      if(!deveria)
+    {
+        resposta = "ótimo";
+        Console.WriteLine(resposta);
+   } 
+   else
+   {
+    resposta = "Use Silver Tape.";
+    Console.WriteLine(resposta);
+
+   }
+}
+
